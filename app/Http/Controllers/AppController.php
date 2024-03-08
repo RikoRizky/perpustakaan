978f083fa5d7b2b44f650500e19c3e5b4f27b1bc
Riko Rizky <rikorizky20@gmail.com>
2024-03-08 08:43:13 +0700

    3

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+
 
 class AppController extends Controller
 {
     //
+
+    public function home(){
+        return view("home");
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function () {
+
+    Route::get("home",[AppController::class,"home"]);
+    Route::get("data",[AppController::class,"data"]);
 });
+
+Route::get("/",[AppController::class,"login"])->name("login");
+Route::post("proses-login",[AuthController::class,"proses_login"]);
+Route::get("logout",[AuthController::class,"proses_logout"]);
