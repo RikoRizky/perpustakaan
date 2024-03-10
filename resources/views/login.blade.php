2ece112a4089e452dca01272d4157dbfe504977a
raysaanindya <raysaecha07@gmail.com>
2024-03-10 23:26:56 +0700

    eca2

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,3 +1,46 @@
 <div>
-    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
+    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
 </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ url('bootstrap.min.css') }}" type="text/css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Login</h4>
+                    </div>
+                    <div class="card-body">
+                    @if(session()->has('message'))
+                     <div class="alert alert-info">
+                        {{ session('message') }}
+                    </div>
+                    @endif
+                        <form action="{{ url('proses-login') }}" method="post">
+                            @csrf
+                            <div class="mb-3">
+                                <label class="form-label">Email</label>
+                                <input type="email" class="form-control" name="email" required>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Password</label>
+                                <input type="password" class="form-control" name="password" required>
+                            </div>
+                            <div class="mb-3">
+                                <button class="btn btn-primary" type="submit">Masuk</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
